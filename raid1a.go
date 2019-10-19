d1ca3cdc40733488319e8f2b483227d61cae6424
Adam <kuzikov.dev@protonmail.com>
2019-10-19 14:09:27 +0600

    raid1a.go fixxx

--- raid1a.go
+++ raid1a.go
@@ -3,22 +3,24 @@ package student
 import "github.com/01-edu/z01"
 
 func Raid1a(w, h int) {
-	for x := 0; x < h; x++ {
-		for y := 0; y < w; y++ {
-			if x == 0 || x == w-1 {
-				if y == 0 || y == h-1 {
-					z01.PrintRune('o')
+	if w < 0 {
+		for x := 0; x < h; x++ {
+			for y := 0; y < w; y++ {
+				if x == 0 || x == w-1 {
+					if y == 0 || y == h-1 {
+						z01.PrintRune('o')
+					} else {
+						z01.PrintRune('-')
+					}
 				} else {
-					z01.PrintRune('-')
-				}
-			} else {
-				if y == 0 || y == h-1 {
-					z01.PrintRune('|')
-				} else {
-					z01.PrintRune(' ')
+					if y == 0 || y == h-1 {
+						z01.PrintRune('|')
+					} else {
+						z01.PrintRune(' ')
+					}
 				}
 			}
+			z01.PrintRune(10)
 		}
-		z01.PrintRune(10)
 	}
 }
