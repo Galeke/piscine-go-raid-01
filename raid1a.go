e07c2c1fe8915dee5a3cc5226d9b875cfac98795
Adam <kuzikov.dev@protonmail.com>
2019-10-19 14:13:13 +0600

    raid1a.go fix3

--- raid1a.go
+++ raid1a.go
@@ -4,6 +4,10 @@ import "github.com/01-edu/z01"
 
 func Raid1a(w, h int) {
 	if w < 0 {
+		for i := 0; i < h; i++ {
+			z01.PrintRune(10)
+		}
+	} else {
 		for x := 0; x < h; x++ {
 			for y := 0; y < w; y++ {
 				if x == 0 || x == w-1 {
